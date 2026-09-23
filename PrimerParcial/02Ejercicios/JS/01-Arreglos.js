4b30e8ea55bccf25ff2f579ddd10ab1dbd25bebc
XavierG14 <jgarciad2407@alumno.ipn.mx>
2026-09-23 11:36:00 -0600

    Resolución de ejercicios de arreglos

--- PrimerParcial/02Ejercicios/JS/01-Arreglos.js
+++ PrimerParcial/02Ejercicios/JS/01-Arreglos.js
@@ -26,7 +26,16 @@ console.log(llenos.map((t)=> t.nombre))
 
 
 // TODO: find — encuentra el PRIMER taller impartido por 'Ing. María López'
+console.log("\nAplicando find en el primer taller de la Ing. María López");
+const tallerMaria = talleres.find((t) => t.instructor === 'Ing. María López');
+console.log(tallerMaria);
 
 // TODO: reduce — calcula `totalInscritos`, la suma de inscritos de todos los talleres
+console.log("\nAplicando reduce en el total de inscritos");
+const totalInscritos = talleres.reduce((acumulador, t) => acumulador + t.inscritos, 0);
+console.log(totalInscritos);
 
 // TODO: filter + map encadenados — nombres de los talleres que SÍ tienen cupo disponible
+console.log("\nAplicando filter en los nombres de talleres con cupo");
+const talleresConCupo = talleres.filter((t) => t.inscritos < t.cupo).map((t) => t.nombre);
+console.log(talleresConCupo);
